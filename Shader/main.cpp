7be502a01c6e2b93cc4d2a72dd2a7b9991045a77
mawanlong <mawanlong@shoyoo.com>
2017-08-23 18:00:09 +0800

    shader init

---
+++ Shader/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  Shader
+//
+//  Created by mwl on 2017/8/23.
+//  Copyright © 2017年 loong6. All rights reserved.
+//
+
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+//    int nrAttributes;
+//    glGetIntegerv(GL_MAX_VERTEX_ATTRIBS,&nrAttributes);
+//    std::cout << "MAX Vertex Attributes:" << nrAttributes << endl;
+    return 0;
+}
