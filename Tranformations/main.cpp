455b22c3d3457712a39d34c7cc9a6253d7e58450
mawanlong <mawanlong@shoyoo.com>
2017-08-24 15:05:14 +0800

    init transformations modify search paths

---
+++ Tranformations/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tranformations
+//
+//  Created by mwl on 2017/8/24.
+//  Copyright © 2017年 loong6. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
